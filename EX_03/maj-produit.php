b30c235cd85158fd8edff92058e90fcc0bb2245f
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-09 11:24:42 +0200

    Exo 3

---
+++ EX_03/maj-produit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+
+    <body>
+        <?php
+            $pdo = connect_to_database(BaseTest01);
+
+            $query = $pdo->query ("SELECT * FROM produit"); 
+            $user = $query->fetch();
+            var_dump($user);
+
+            $users = $pdo->query("SELECT * FROM produit")->fetchAl1();
+
+            var_dump($users); 
+            foreach ($users as $user) { 
+                echo $user[ 'produit']."<br/>"; 
+            }
+            
+            UPDATE "produit" ;
+            SET quantite = 1 ;
+            WHERE id = "Short Bleu";
+
+        ?>
+    </body>
+</html>
